65cca65fed566330f50ef59b54d74292a7f0e7ec
gb051406 <scooterboy06@icloud.com>
2024-01-12 11:12:19 -0600

    stuff added

--- tkinter-project.py
+++ tkinter-project.py
@@ -2,7 +2,11 @@ import tkinter as tk
 
 #functions
 def compute():
-
+    try:
+        tanAcc = (radius.get())*(acc.get())
+        centrip = (speed**2)/(radius)
+    except ValueError:
+        
 #window config
 canvas = tk.Tk()
 canvas.geometry("400x400")
@@ -11,6 +15,7 @@ canvas.config(bg = "light blue")
 #labels
 radii = tk.Label(canvas, text="Enter radius:")
 velocity = tk.Label(canvas, text="Enter velocity:")
+acceleration = tk.Label(canvas, text="Enter acceleration:")
 #text
 radius = tk.Entry(canvas)
 speed = tk.Entry(canvas)
