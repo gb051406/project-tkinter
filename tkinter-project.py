8f20b303b0cc6a0b96633ab9c482e6b6b792e8f5
gb051406 <scooterboy06@icloud.com>
2024-01-17 10:43:21 -0600

    added extra things

--- tkinter-project.py
+++ tkinter-project.py
@@ -3,40 +3,46 @@ import tkinter as tk
 #functions
 def compute():
     try:
-        tanAcc = (float(txtradius.get()))*(float(txtacc.get())) #calculations
-        centrip = (float(txtspeed.get())**2)/(float(txtradius.get()))
-        lblsolutionOne.config(text="Tangential Acceleration = "+str(tanAcc)) #changes solution text to solution
-        lblsolutionTwo.config(text="Centripetal Acceleration = "+str(centrip))
-    except ValueError: #allows for no stopage when entering numbers
-        lblsolutionOne.config(text="ERROR: enter only numbers")
+        tanAcc = (float(txtRadius.get()))*(float(txtAcc.get())) #calculations
+        centrip = (float(txtSpeed.get())**2)/(float(txtRadius.get()))
+        lblSolutionOne.config(text="Tangential Acceleration = "+str(tanAcc)) #changes solution text to solution
+        lblSolutionTwo.config(text="Centripetal Acceleration = "+str(centrip))
+    except ValueError: #allows for no stopage when entering letters, not numbers
+        lblSolutionOne.config(text="ERROR: enter only numbers")
 #window config
 canvas = tk.Tk()
 canvas.geometry("400x400")
 canvas.title("something")
 canvas.config(bg = "light blue")
 #labels
-lblradii = tk.Label(canvas, text="Enter radius:")
-lblvelocity = tk.Label(canvas, text="Enter velocity:")
-lblacceleration = tk.Label(canvas, text="Enter acceleration:")
-lblsolutionOne = tk.Label(canvas, text="")
-lblsolutionTwo = tk.Label(canvas, text="")
+lblRadius = tk.Label(canvas, text="Enter radius of the circle:")
+lblVelocity = tk.Label(canvas, text="Enter angular velocity:")
+lblAcceleration = tk.Label(canvas, text="Enter angular acceleration:")
+lblSolutionOne = tk.Label(canvas, text="")
+lblSolutionTwo = tk.Label(canvas, text="")
 #text
-txtradius = tk.Entry(canvas)
-txtspeed = tk.Entry(canvas)
-txtacc = tk.Entry(canvas)
+txtRadius = tk.Entry(canvas)
+txtSpeed = tk.Entry(canvas)
+txtAcc = tk.Entry(canvas)
 
 #button
-btncalc = tk.Button(canvas, text="Calculate", command=compute)
+btnCalc = tk.Button(canvas, text="Calculate", command=compute)
 
 #GUI
-lblradii.grid(row=0,column=0,padx=10,pady=10)
-txtradius.grid(row=0,column=1,padx=10,pady=10)
-lblvelocity.grid(row=1,column=0,padx=10,pady=10)
-txtspeed.grid(row=1,column=1,padx=10,pady=10)
-lblacceleration.grid(row=2,column=0,padx=10,pady=10)
-txtacc.grid(row=2,column=1,padx=10,pady=10)
-btncalc.grid(row=3,column=0,padx=10,pady=10)
-lblsolutionOne.grid(row=3,column=1,padx=10,pady=10)
-lblsolutionTwo.grid(row=4,column=1,padx=10,pady=10)
-#run
+lblRadius.grid(row=0,column=0,padx=10,pady=10)
+txtRadius.grid(row=0,column=1,padx=10,pady=10)
+lblVelocity.grid(row=1,column=0,padx=10,pady=10)
+txtSpeed.grid(row=1,column=1,padx=10,pady=10)
+lblAcceleration.grid(row=2,column=0,padx=10,pady=10)
+txtAcc.grid(row=2,column=1,padx=10,pady=10)
+btnCalc.grid(row=3,column=0,padx=10,pady=10)
+lblSolutionOne.grid(row=3,column=1,padx=10,pady=10)
+lblSolutionTwo.grid(row=4,column=1,padx=10,pady=10)
+#label, text, and button config, for background color
+lblRadius.config(bg="light blue")
+lblVelocity.config(bg="light blue")
+lblAcceleration.config(bg="light blue")
+lblSolutionOne.config(bg="light blue")
+lblSolutionTwo.config(bg="light blue")
+#builds the window
 canvas.mainloop()
