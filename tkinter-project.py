cd2756f4d8e6b0237b3dfe4966517262ca56b148
gb051406 <scooterboy06@icloud.com>
2024-01-17 10:28:57 -0600

    added GUI

--- tkinter-project.py
+++ tkinter-project.py
@@ -3,29 +3,40 @@ import tkinter as tk
 #functions
 def compute():
     try:
-        tanAcc = (radius.get())*(acc.get())
-        centrip = (speed**2)/(radius)
+        tanAcc = (float(txtradius.get()))*(float(txtacc.get()))
+        centrip = (float(txtspeed.get())**2)/(float(txtradius.get()))
+        lblsolutionOne.config(text="Tangential Acceleration = "+str(tanAcc))
+        lblsolutionTwo.config(text="Centripetal Acceleration = "+str(centrip))
     except ValueError:
-        
+        lblsolutionOne.config(text="ERROR: enter only numbers")
 #window config
 canvas = tk.Tk()
 canvas.geometry("400x400")
 canvas.title("something")
 canvas.config(bg = "light blue")
 #labels
-radii = tk.Label(canvas, text="Enter radius:")
-velocity = tk.Label(canvas, text="Enter velocity:")
-acceleration = tk.Label(canvas, text="Enter acceleration:")
+lblradii = tk.Label(canvas, text="Enter radius:")
+lblvelocity = tk.Label(canvas, text="Enter velocity:")
+lblacceleration = tk.Label(canvas, text="Enter acceleration:")
+lblsolutionOne = tk.Label(canvas, text="")
+lblsolutionTwo = tk.Label(canvas, text="")
 #text
-radius = tk.Entry(canvas)
-speed = tk.Entry(canvas)
-acc = tk.Entry(canvas)
+txtradius = tk.Entry(canvas)
+txtspeed = tk.Entry(canvas)
+txtacc = tk.Entry(canvas)
 
 #button
-clac = tk.Button(canvas, text="Calculate", command=compute)
+btncalc = tk.Button(canvas, text="Calculate", command=compute)
 
 #GUI
-
-
+lblradii.grid(row=0,column=0,padx=10,pady=10)
+txtradius.grid(row=0,column=1,padx=10,pady=10)
+lblvelocity.grid(row=1,column=0,padx=10,pady=10)
+txtspeed.grid(row=1,column=1,padx=10,pady=10)
+lblacceleration.grid(row=2,column=0,padx=10,pady=10)
+txtacc.grid(row=2,column=1,padx=10,pady=10)
+btncalc.grid(row=3,column=0,padx=10,pady=10)
+lblsolutionOne.grid(row=3,column=1,padx=10,pady=10)
+lblsolutionTwo.grid(row=4,column=1,padx=10,pady=10)
 #run
 canvas.mainloop()
